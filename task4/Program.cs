ca364c78a6102409ae8740f960569b499b3d97b1
ViVBelous <ViVBelous@1cbit.ru>
2025-05-19 17:44:39 +0300

    Проект vs с файлом .gitigonre

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 2;
+            int sum = a + b;
+            Console.WriteLine($"Сумма: {sum}");
+        }
+    }
+}
